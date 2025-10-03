be4b4b3584a700cb6851205d868bcc4a99a67b8f
Raghav-Code-dt <rg06042006@gmail.com>
2025-10-04 00:18:42 +0530

    Update content in home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -144,7 +144,7 @@ export default function Home() {
         >
           Our AI-powered platform doesn’t just track your steps. It gives you
           real wellness insights — life expectancy estimates, personalized diet
-          & exercise plans, and disease risk predictions
+          & exercise plans, and disease risk predictions and will lead to a healthier life .
         </motion.p>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-5xl">
